e6e4c3a3960265c928f2b64af941a8c203a71eeb
Sanguk Lee <dltkddnr04@icloud.com>
2022-09-04 22:44:05 +0900

    계속 GUI 하는중

--- heartbeat_detector.py
+++ heartbeat_detector.py
@@ -15,7 +15,7 @@ class GUI(QWidget):
         grid.addWidget(self.program_settings(), 0, 1)
         self.setLayout(grid)
         self.resize(600, 300)
-        self.setWindowTitle('Can you feel my heartbeat?')
+        self.setWindowTitle('Heartbeat Detector')
         self.show()
 
     def camera_display(self):
@@ -37,22 +37,75 @@ class GUI(QWidget):
         groupbox = QGroupBox('설정')
         grid = QGridLayout()
 
+        grid.addWidget(self.camera_settings(), 0, 0)
+        grid.addWidget(self.status_widget(), 1, 0)
+        grid.addWidget(self.passthrough_settings(), 2, 0)
+        
+        groupbox.setLayout(grid)
+        return groupbox
+
+    def camera_settings(self):
+        groupbox = QGroupBox('카메라 선택')
+        grid = QGridLayout()
+
         self.select_camera = QComboBox(self)
         self.select_camera.addItem('1')
         self.select_camera.addItem('2')
         self.select_camera.addItem('3')
         self.select_camera.addItem('4')
 
-        self.passthrough_start = qtwid.QPushButton("웹캠 패스스루 시작",self)
-        self.info_graphic_start = qtwid.QPushButton("그래픽 출력 시작",self)
+        grid.addWidget(self.select_camera, 1, 0)
+        groupbox.setLayout(grid)
+        return groupbox
 
-        grid.addWidget(self.select_camera, 0, 0, 1, 0)
-        grid.addWidget(self.passthrough_start, 1, 0)
-        grid.addWidget(self.info_graphic_start, 2, 0)
-        
+    def status_widget(self):
+        groupbox = QGroupBox('상태')
+        grid = QGridLayout()
+
+        self.tracking_status = QLabel('트래킹 상태 {}'.format('🟢'))
+        self.heartbeat_rate = QLabel('심박수 {}bpm'.format(0))
+
+        grid.addWidget(self.tracking_status, 0, 0)
+        grid.addWidget(self.heartbeat_rate, 1, 0)
 
         groupbox.setLayout(grid)
         return groupbox
+
+    def passthrough_settings(self):
+        groupbox = QGroupBox('패스스루')
+        grid = QGridLayout()
+
+        self.passthrough_button = qtwid.QPushButton("웹캠 출력 시작",self)
+        self.info_graphic_button = qtwid.QPushButton("그래픽 출력 시작",self)
+
+        grid.addWidget(self.passthrough_button, 1, 0)
+        grid.addWidget(self.info_graphic_button, 2, 0)
+
+        self.passthrough_button.clicked.connect(self.passthrough_start)
+        self.info_graphic_button.clicked.connect(self.info_graphic_start)
+
+        groupbox.setLayout(grid)
+        return groupbox
+
+    def passthrough_start(self):
+        self.passthrough_button.setText("웹캠 출력 중지")
+        self.passthrough_button.clicked.connect(self.passthrough_stop)
+        return
+
+    def info_graphic_start(self):
+        self.info_graphic_button.setText("그래픽 출력 중지")
+        self.info_graphic_button.clicked.connect(self.info_graphic_stop)
+        return
+
+    def passthrough_stop(self):
+        self.passthrough_button.setText("웹캠 출력 시작")
+        self.passthrough_button.clicked.connect(self.passthrough_start)
+        return
+
+    def info_graphic_stop(self):
+        self.info_graphic_button.setText("그래픽 출력 시작")
+        self.info_graphic_button.clicked.connect(self.info_graphic_start)
+        return
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
